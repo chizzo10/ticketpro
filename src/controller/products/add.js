a41dc221b6bd557b44b00348b60c327afe2f2426
Roberto Zalazar <servicios773@gmail.com>
2023-08-25 17:08:22 -0300

    editando

---
+++ src/controller/products/add.js
@@ -0,0 +1,11 @@
+const { readJSON } = require('../../data')
+
+module.exports = (req,res) => {
+    const categories = readJSON('categories.json');
+    
+    const sections = readJSON('sections.json');
+    return res.render('addProduct',{
+        categories,
+        sections : sections.sort()
+    })
+}
---
+++ src/controller/products/create.js
@@ -0,0 +1,11 @@
+const Product = require('../../data/Product');
+const { readJSON, writeJSON } = require('../../data');
+
+module.exports = (req,res) => {
+    const products = readJSON('products.json');
+
+    const newProduct = new Product(req.body);
+    products.push(newProduct);
+    writeJSON(products, 'products.json');
+    return res.redirect('/')
+}
